ee9f6d17821cce0d1eb7e1d1a174d7773e4c5c9d
PhiBo <phibo@dinotools.org>
2014-10-19 09:15:02 +0200

    src - Improve SSLv2 record parsing
    * Support 2 and 3 byte headers
    * Detect ClientHello and ServerHello payload

--- flextls/protocol/record.py
+++ flextls/protocol/record.py
@@ -1,12 +1,14 @@
 """
 The SSL/TLS Record Protocol
 """
+import struct
+
 
 from flextls.field import UByteEnumField, UShortField, VersionField
 from flextls.protocol import Protocol
 from flextls.protocol.alert import Alert
 from flextls.protocol.change_cipher_spec import ChangeCipherSpec
-from flextls.protocol.handshake import Handshake
+from flextls.protocol.handshake import Handshake, SSLv2ClientHello, SSLv2ServerHello
 from flextls.protocol.heartbeat import Heartbeat
 
 
@@ -30,21 +32,41 @@ class RecordSSLv2(Protocol):
 
     def __init__(self, **kwargs):
         Protocol.__init__(self, **kwargs)
-        self.fields = [
-            UShortField("length", 0),
-            UByteEnumField(
-                "type",
-                None,
-                {
-                    1: "client_hello",
-                    255: None
-                }
-            ),
-            VersionField("version"),
-        ]
+        self.length = 0
+        self.is_escape = None
+        self.padding_length = None
+        self.padding = b""
+        self.type = None
 
     def dissect(self, data):
-        data = Protocol.dissect(self, data)
+        # Is it 2 or 3 bytes header
+        if struct.unpack("!B", data[:1])[0] & 0x80 == 0:
+            tmp = struct.unpack("!BBB", data[:3])
+            data = data[3:]
+            self.length = ((tmp[0] & 0x3f) << 8) | tmp[1]
+            self.is_escape = ((tmp[0] & 0x40) != 0)
+            self.padding_length = tmp[2]
+        else:
+            tmp = struct.unpack("!BB", data[:2])
+            data = data[2:]
+            self.length = ((tmp[0] & 0x7f) << 8) | tmp[1]
+
+        self.type = struct.unpack("!B", data[:1])[0]
+        data = data[1:]
+
+        payload_class = self.payload_list.get(
+            self.type,
+            None
+        )
+
+        if payload_class is None:
+            self.payload = data
+        else:
+            (obj, data) = payload_class.decode(
+                data,
+                connection_state=self._connection_state
+            )
+            self.payload = obj
 
         return data
 
@@ -77,6 +99,8 @@ class RecordSSLv3(Protocol):
         return data
 
 
+RecordSSLv2.add_payload_type(1, SSLv2ClientHello)
+RecordSSLv2.add_payload_type(4, SSLv2ServerHello)
 RecordSSLv3.add_payload_type(20, ChangeCipherSpec)
 RecordSSLv3.add_payload_type(21, Alert)
 RecordSSLv3.add_payload_type(22, Handshake)
