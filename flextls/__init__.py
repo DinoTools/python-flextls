f589111ccbc6e691583b72d5cb6ff373e7244595
PhiBo <phibo@dinotools.org>
2014-11-17 21:40:52 +0100

    src - Import about information

--- flextls/__init__.py
+++ flextls/__init__.py
@@ -1,4 +1,8 @@
 from flextls._registry import Registry
+from sslscan.__about__ import (
+    __author__, __copyright__, __email__, __license__, __summary__, __title__,
+    __uri__, __version__
+)
 
 
 registry = Registry()
