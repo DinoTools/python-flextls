ebf6b3bb65816feadbb716e5ec9a61f277396b2b
PhiBo <phibo@dinotools.org>
2015-02-06 23:51:55 +0100

    src - New helper function to get version from id

--- flextls/helper.py
+++ flextls/helper.py
@@ -1,6 +1,14 @@
 from flextls import registry
 
 
+def get_version_by_version_id(version_id):
+    for ver in registry.version_info:
+        if ver.version_id == version_id:
+            return ver.id
+
+    return None
+
+
 def get_version_name(protocol_version):
     ver = registry.version_info.get(protocol_version)
     if ver:
