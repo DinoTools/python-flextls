774c331979a1efb4d28e29e3ef87a0446a5e3513
PhiBo <phibo@dinotools.org>
2015-02-06 23:51:24 +0100

    src - Add new exception

--- flextls/exception.py
+++ flextls/exception.py
@@ -1,2 +1,8 @@
 class NotEnoughData(IOError):
     pass
+
+
+class WrongProtocolVersion(IOError):
+    def __init__(self, *args, record=None):
+        IOError.__init__(self, *args)
+        self.record = record
