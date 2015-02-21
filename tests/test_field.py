ba0c94a87b450a07fd891236456ec9f7808d6150
PhiBo <phibo@dinotools.org>
2015-02-21 22:25:12 +0100

    test - Tests for enum fields

--- tests/test_field.py
+++ tests/test_field.py
@@ -138,3 +138,94 @@ class TestNumberFields(object):
         for i in range(0, 32):
             with pytest.raises(NotEnoughData):
                 f.dissect(b"A"*i)
+
+
+class TestEnumFields(object):
+    def test_uint8enumfield(self):
+        f = UInt8EnumField(
+            "test",
+            0,
+            {
+                0: "v_000",
+                128: "v_128"
+            }
+        )
+
+        assert f.value == 0
+        assert f.assemble() == b"\x00"
+        assert f.get_value_name() == "v_000"
+
+        f.value = 1
+        assert f.assemble() == b"\x01"
+        assert f.get_value_name() == "n/a"
+
+        f.value = 128
+        assert f.assemble() == b"\x80"
+        assert f.get_value_name() == "v_128"
+        assert f.get_value_name(True).startswith("v_128")
+        assert f.get_value_name() != f.get_value_name(True)
+
+        f.value = "v_000"
+        assert f.value == 0
+
+        with pytest.raises(ValueError):
+            f.value = "v_001"
+
+        assert f.value == 0
+
+        with pytest.raises(TypeError):
+            f.value = []
+
+        assert f.dissect(b"\x80") == b""
+        assert f.value == 128
+
+        assert f.dissect(b"\x00\x99") == b"\x99"
+
+        with pytest.raises(NotEnoughData):
+            f.dissect(b"")
+
+    def test_uint16enumfield(self):
+        f = UInt16EnumField(
+            "test",
+            0,
+            {
+                0: "v_00000",
+                32768: "v_32768"
+            }
+        )
+
+        assert f.value == 0
+        assert f.assemble() == b"\x00\x00"
+        assert f.get_value_name() == "v_00000"
+
+        f.value = 1
+        assert f.assemble() == b"\x00\x01"
+        assert f.get_value_name() == "n/a"
+
+        f.value = 32768
+        assert f.assemble() == b"\x80\x00"
+        assert f.get_value_name() == "v_32768"
+        assert f.get_value_name(True).startswith("v_32768")
+        assert f.get_value_name() != f.get_value_name(True)
+
+        f.value = "v_00000"
+        assert f.value == 0
+
+        with pytest.raises(ValueError):
+            f.value = "v_00001"
+
+        assert f.value == 0
+
+        with pytest.raises(TypeError):
+            f.value = []
+
+        assert f.dissect(b"\x80\00") == b""
+        assert f.value == 32768
+
+        assert f.dissect(b"\x00\x00\x99") == b"\x99"
+
+        with pytest.raises(NotEnoughData):
+            f.dissect(b"")
+
+        with pytest.raises(NotEnoughData):
+            f.dissect(b"\x00")
