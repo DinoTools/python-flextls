c1b0ff5a549283e1a7040f39ad697e3919edbb0c
PhiBo <phibo@dinotools.org>
2015-07-25 00:49:09 +0200

    test - Add support for Next Protocol Negotiation extension (Ref: #13)

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,7 +1,7 @@
 import binascii
 
 from flextls.protocol.record import Record
-from flextls.protocol.handshake.extension import Extension, SessionTicketTLS, ServerNameIndication, ApplicationLayerProtocolNegotiation
+from flextls.protocol.handshake.extension import Extension, SessionTicketTLS, ServerNameIndication, ApplicationLayerProtocolNegotiation, NextProtocolNegotiation
 from flextls.field import ServerNameField, HostNameField, VectorUInt8Field
 
 
@@ -49,44 +49,63 @@ class TestApplicationLayerProtocolNegotiation(object):
 class TestNextProtocolNegotiation(object):
     @staticmethod
     def _get_data():
-        # Type: Next Protocol Negotiation, Length: 25
+        # Type: Next Protocol Negotiation, Length: 40
         data = b"33740028"
-        data += b"0268320568322d31350568322d313408737064792f332e3106737064792f3308687474702f312e31"
-        data = b"00100019"
-        # ALP Extensioin Length: 23
-        data += b"0017"
+        # Length: 2, Name: h2
+        data += b"026832"
+        # Length: 5, Name: h2-15
+        data += b"0568322d3135"
+        # Length: 5, Name: h2-14
+        data += b"0568322d3134"
+        # Length: 8, Name: spdy/3.1
+        data += b"08737064792f332e31"
         # Length: 6, Name: spdy/3
         data += b"06737064792f33"
-        # Length: 6, Name: spdy/2
-        data += b"06737064792f32"
         # Length: 8, Name: http/1.1
         data += b"08687474702f312e31"
         return data
 
     def test_encode(self):
-        tmp = Extension() + ApplicationLayerProtocolNegotiation()
+        tmp = Extension() + NextProtocolNegotiation()
+        a = VectorUInt8Field(None)
+        a.value = b"h2"
+        tmp.payload.payload.append(a)
+        a = VectorUInt8Field(None)
+        a.value = b"h2-15"
+        tmp.payload.payload.append(a)
+        a = VectorUInt8Field(None)
+        a.value = b"h2-14"
+        tmp.payload.payload.append(a)
+        a = VectorUInt8Field(None)
+        a.value = b"spdy/3.1"
+        tmp.payload.payload.append(a)
         a = VectorUInt8Field(None)
         a.value = b"spdy/3"
-        tmp.payload.protocol_name_list.append(a)
-        a = VectorUInt8Field(None)
-        a.value = b"spdy/2"
-        tmp.payload.protocol_name_list.append(a)
+        tmp.payload.payload.append(a)
         a = VectorUInt8Field(None)
         a.value = b"http/1.1"
-        tmp.payload.protocol_name_list.append(a)
+        tmp.payload.payload.append(a)
 
         data = tmp.encode()
         data_should = self._get_data()
         assert binascii.hexlify(data) == data_should
 
+    def test_encode_empty(self):
+        tmp = Extension() + NextProtocolNegotiation()
+        data = tmp.encode()
+        assert binascii.hexlify(data) == b"33740000"
+
     def test_decode(self):
         data = self._get_data()
 
         (obj, data) = Extension.decode(binascii.unhexlify(data))
-        assert len(obj.payload.protocol_name_list) == 3
-        assert obj.payload.protocol_name_list[0].value == b"spdy/3"
-        assert obj.payload.protocol_name_list[1].value == b"spdy/2"
-        assert obj.payload.protocol_name_list[2].value == b"http/1.1"
+        assert len(obj.payload.payload) == 6
+        assert obj.payload.payload[0].value == b"h2"
+        assert obj.payload.payload[1].value == b"h2-15"
+        assert obj.payload.payload[2].value == b"h2-14"
+        assert obj.payload.payload[3].value == b"spdy/3.1"
+        assert obj.payload.payload[4].value == b"spdy/3"
+        assert obj.payload.payload[5].value == b"http/1.1"
 
 class TestSessionTicketTLS(object):
 
